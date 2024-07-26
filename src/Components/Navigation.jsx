81c343d3acb39e094451b7fe0c54d57be106af94
Taurius <ytaurius159@email.com>
2024-07-26 10:29:44 +0300

    header styled

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,18 +1,36 @@
 import { Link } from "react-router-dom";
 
 function Navigation({ user, handleLogout }) {
-  console.log(user);
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      {user && <Link to="/activityTimeTracker">Activity Time Tracker App</Link>}
-      {user ? (
-        <button onClick={handleLogout}>Logout</button>
-      ) : (
-        <Link to="/login">Login</Link>
-      )}
-      {!user && <Link to="/register">Register</Link>}
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          {user && (
+            <li>
+              <Link to="/activityTimeTracker">Activity Time Tracker App</Link>
+            </li>
+          )}
+          {user ? (
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          )}
+          {!user && (
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          )}
+        </ul>
+      </nav>
+      <h1>Activity Time Tracker</h1>
+    </header>
   );
 }
 
